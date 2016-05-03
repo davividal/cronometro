ce7435f7541d78850f58dd9f70857c3e6af6e25e
Davi Koscianski Vidal <davividal@gmail.com>
2016-05-03 20:51:05 -0300

    40% pronto

--- app/src/main/java/br/com/davividal/cronometro/MainActivity.java
+++ app/src/main/java/br/com/davividal/cronometro/MainActivity.java
@@ -1,13 +1,43 @@
 package br.com.davividal.cronometro;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler cronometro = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view) {
+        TextView timer = (TextView) findViewById(R.id.timer);
+
+        assert timer != null;
+
+        cronometro.postDelayed(new Runnable() {
+            public TextView timer;
+
+            @Override
+            public void run() {
+                Integer actual = Integer.parseInt((String) timer.getText());
+                timer.setText(String.format(new Locale("pt", "br"), "%d", ++actual));
+            }
+
+            public Runnable init(TextView timer) {
+                this.timer = timer;
+                return this;
+            }
+        }.init(timer), 1000);
+    }
+
+    public void stop(View view) {
     }
 }
